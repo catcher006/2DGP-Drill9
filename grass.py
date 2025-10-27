c4287e490ca981735dac4422b89d1f49074fe2a5
catcher006 <seungho217@tukorea.ac.kr>
2025-10-27 12:54:14 +0900

    grass y좌표 2개로 분리및 time으로 조정 (이전 self.y -= 30은 계속 내려가 사라지므로 time으로 조정함)

--- grass.py
+++ grass.py
@@ -5,10 +5,16 @@ class Grass:
     def __init__(self):
         self.image = load_image('grass.png')
         self.x = 400
-        self.y = 60
+        self.y1 = 6
+        self.y2 = 30
+        self.time = 0
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        if self.time % 2 == 0:
+            self.image.draw(self.x, self.y1)
+        else:
+            self.image.draw(self.x, self.y2)
+        self.time += 1
 
     def update(self):
         pass
